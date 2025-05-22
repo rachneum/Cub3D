c7172a0d8bedc1a8ee909ecdf5d5b34a364fe497
Klaenex <klaenex.29@gmail.com>
2025-05-22 18:29:36 +0200

    test

--- parsing/check_texture.c
+++ parsing/check_texture.c
@@ -10,14 +10,70 @@ static int	check_textures_path(char *path)//Vérifie chemin vers la texture et f
 		ft_error("ERROR: Opening texture file failure!\n");
 		close (fd);
 	}
-	//Check si path n'existe pas
 	len = ft_strlen(path);
 	if ((ft_strncmp(path + len - 4, ".xpm", 4)) != 0)
-		ft_error("ERROR: Texture format is invalid!\n");//->Vérifie format fichier texture.
+		ft_error("ERROR: Texture format is invalid!\n");//Vérifie format fichier texture.
 	return (0);
 }
 
-int	check_texture(t_data *game, char *line)//Protéger path dans mes conditions au cas où il est vide!
+static int	check_east(t_data *game, char *line)
+{
+	char	*path;
+
+	if (ft_strncmp(line, "EA ", 3) == 0)
+	{
+		if (game->texture[3] != NULL)
+			ft_error("ERROR: Duplicate EA texture!\n");
+		path = ft_strtrim(line + 3, " \n");
+		if (check_textures_path(path))
+	    	ft_error("ERROR: Invalid texture path");
+		game->texture[3] = ft_strdup(path);
+		free(path);
+		printf ("%s\n", line);
+		return (1);
+	}
+	return (0);
+}
+
+static int check_west(t_data *game, char *line)
+{
+	char	*path;
+
+	if (ft_strncmp(line, "WE ", 3) == 0)
+	{
+		if (game->texture[2] != NULL)
+			ft_error("ERROR: Duplicate WE texture!\n");
+		path = ft_strtrim(line + 3, " \n");
+		if (check_textures_path(path))
+	    	ft_error("ERROR: Invalid texture path");
+		game->texture[2] = ft_strdup(path);
+		free(path);
+		printf ("%s\n", line);
+		return (1);
+	}
+	return (0);
+}
+
+static int check_south(t_data *game, char *line)
+{
+	char	*path;
+
+	if (ft_strncmp(line, "SO ", 3) == 0)
+	{
+		if (game->texture[1] != NULL)
+			ft_error("ERROR: Duplicate SO texture!\n");
+		path = ft_strtrim(line + 3, " \n");
+		if (check_textures_path(path))
+	    	ft_error("ERROR: Invalid texture path");
+		game->texture[1] = ft_strdup(path);
+		free(path);
+		printf ("%s\n", line);
+		return (1);
+	}
+	return (0);
+}
+
+static int check_north(t_data *game, char *line)
 {
 	char	*path;
 
@@ -33,42 +89,20 @@ int	check_texture(t_data *game, char *line)//Protéger path dans mes conditions 
 		printf ("%s\n", line);
 		return (1);
 	}
-	else if (ft_strncmp(line, "SO ", 3) == 0)
-	{
-		if (game->texture[1] != NULL)
-			ft_error("ERROR: Duplicate SO texture!\n");
-		path = ft_strtrim(line + 3, " \n");
-		if (check_textures_path(path))
-	    	ft_error("ERROR: Invalid texture path");
-		game->texture[1] = ft_strdup(path);
-		free(path);
-		printf ("%s\n", line);
+
+	return (0);
+}
+
+int	check_texture(t_data *game, char *line)
+{
+	if (check_north(game, line))
 		return (1);
-	}
-	else if (ft_strncmp(line, "WE ", 3) == 0)
-	{
-		if (game->texture[2] != NULL)
-			ft_error("ERROR: Duplicate WE texture!\n");
-		path = ft_strtrim(line + 3, " \n");
-		if (check_textures_path(path))
-	    	ft_error("ERROR: Invalid texture path");
-		game->texture[2] = ft_strdup(path);
-		free(path);
-		printf ("%s\n", line);
+	if (check_south(game, line))
 		return (1);
-	}
-	else if (ft_strncmp(line, "EA ", 3) == 0)
-	{
-		if (game->texture[3] != NULL)
-			ft_error("ERROR: Duplicate EA texture!\n");
-		path = ft_strtrim(line + 3, " \n");
-		if (check_textures_path(path))
-	    	ft_error("ERROR: Invalid texture path");
-		game->texture[3] = ft_strdup(path);
-		free(path);
-		printf ("%s\n", line);
+	if (check_west(game, line))
 		return (1);
-	}
+	if (check_east(game, line))
+		return (1);
 	if (ft_strncmp(line, "F", 1) == 0 || ft_strncmp(line, "C", 1) == 0)
 	{
 		return (check_color(game, line));
