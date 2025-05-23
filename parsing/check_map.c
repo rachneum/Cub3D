61957bea1355a7316b61ace7df37cd89e50825e0
Klaenex <klaenex.29@gmail.com>
2025-05-23 23:53:45 +0200

    test

--- parsing/check_map.c
+++ parsing/check_map.c
@@ -3,11 +3,11 @@
 static int	flood_fill(t_data *game, int y, int x)
 {
 	if (y < 0 || x < 0 || !game->map_copy[y] || x >= (int)ft_strlen(game->map_copy[y]))
-		return (1);//Fuite détectée.
+		return (1);
 	if (game->map_copy[y][x] == ' ' || game->map_copy[y][x] == '\t' || game->map_copy[y][x] == '\0')
-		return (1);//Fuite détectée.
+		return (1);
 	if (game->map_copy[y][x] == '1' || game->map_copy[y][x] == 'X')
-		return (0);//Mur ou déjà visité.
+		return (0);
 	game->map_copy[y][x] = 'X';//Marquer comme visité.
 	if (flood_fill(game, y - 1, x))
 		return (1);
